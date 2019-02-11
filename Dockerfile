b657f1ecac761cb76c90fbcbf5e5c8a501b3e7ad
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-02-11 17:55:20 +0200

    Refs #101376; Wip on switching to garie-plugin package

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,20 @@
 FROM node:8.10.0
 
-RUN mkdir -p /usr/src/garie-lighthouse
-WORKDIR /usr/src/garie-lighthouse
+RUN mkdir -p /usr/src/garie-plugin
+RUN mkdir -p /usr/src/garie-plugin/reports
+
+WORKDIR /usr/src/garie-plugin
 
 COPY package.json .
 
-RUN npm install
-
-# Install Google Chrome
-RUN \
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-    sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
-    apt-get update && \
-    apt-get install -y google-chrome-stable && \
-    wget https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64.deb && \
-    dpkg -i dumb-init_*.deb
+RUN cd /usr/src/garie-plugin && npm install
 
 COPY . .
 
-COPY docker-entrypoint.sh /
-
 EXPOSE 3000
 
-VOLUME ["/usr/src/garie-lighthouse/reports", "/usr/src/garie-lighthouse/logs"]
+VOLUME ["/usr/src/garie-plugin/reports"]
 
-ENTRYPOINT ["/docker-entrypoint.sh"]
+ENTRYPOINT ["/usr/src/garie-plugin/docker-entrypoint.sh"]
 
-CMD ["/usr/bin/dumb-init", "npm", "start"]
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -1,81 +1,62 @@
-const CronJob = require('cron').CronJob;
-const express = require('express');
-const serveIndex = require('serve-index');
-const bodyParser = require('body-parser');
-
-const collect = require('./routes/collect');
-const logger = require('./utils/logger');
-const saveReport = require('./utils/save-report');
+const garie_plugin = require('garie-plugin')
+const path = require('path');
 const config = require('../config');
-const { getData } = require('./light-house');
+const express = require('express');
+const bodyParser = require('body-parser');
+const serveIndex = require('serve-index');
 
-const { init, saveData } = require('./influx');
-
-const app = express();
-app.use(bodyParser.json());
-
-const { urls, cron } = config;
-
-app.use('/collect', collect);
-app.use('/reports', express.static('reports'), serveIndex('reports', { icons: true }));
-
-const getDataForAllUrls = async () => {
-    // Run lighthouse tests 1 after another.... maybe parallel one day?
-    for (const item of urls) {
-        try {
-            const { url, plugins = [] } = item;
-
-            const pluginConfig = plugins.find(({ name }) => {
-                return name === 'lighthouse';
-            });
-
-            const { report } = pluginConfig || {};
-
-            const { raw, filteredData } = (await getData(item.url)) || {};
-
-            await saveData(item.url, filteredData);
-
-            if (report) {
-                await saveReport(url, raw);
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    logger.info('Finished processed all CRON urls');
-};
-
-const main = async () => {
-    await init();
-
-    try {
-        if (cron) {
-            return new CronJob(
-                cron,
-                async () => {
-                    getDataForAllUrls();
-                },
-                null,
-                true,
-                'Europe/London',
-                null,
-                true
-            );
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-if (process.env.ENV !== 'test') {
-    app.listen(3000, async () => {
-        console.log('Application listening on port 3000');
-        await main();
-    });
+const myGetFile = async (options) => {
+    options.fileName = '<my_garie_plugin>.txt';
+    const file = await garie_plugin.utils.helpers.getNewestFile(options);
+    return getResults(file);
 }
 
-module.exports = {
-    main,
-    app
+const myGetData = async (item) => {
+    const { url } = item.url_settings;
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { extra_option } = item.url_settings;
+            const { reportDir } = item;
+
+            const options = { script: path.join(__dirname, './my_script.sh'),
+                        url: url,
+                        reportDir: reportDir,
+                        params: [ extra_option ],
+                        callback: myGetFile
+                    }
+            data = await garie_plugin.utils.helpers.executeScript(options);
+
+// my code to get the data for a url
+
+            resolve(data);
+        } catch (err) {
+            console.log(`Failed to get data for ${url}`, err);
+            reject(`Failed to get data for ${url}`);
+        }
+    });
 };
+
+
+
+console.log("Start");
+
+
+const app = express();
+app.use('/reports', express.static('reports'), serveIndex('reports', { icons: true }));
+
+const main = async () => {
+  garie_plugin.init({
+    database:'lighthouse',
+    getData:myGetData,
+    app_name:'lighthouse-reports',
+    app_root: path.join(__dirname, '..'),
+    config:config
+  });
+}
+
+if (process.env.ENV !== 'test') {
+  app.listen(3000, async () => {
+    console.log('Application listening on port 3000');
+    await main();
+  });
+}
