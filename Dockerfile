8352aa95b5fc40c43514a182b8e86d320180b113
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-02-13 17:54:19 +0200

    Refs #101376; Updated plugin to use garie-plugin package

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,15 @@ COPY package.json .
 
 RUN cd /usr/src/garie-plugin && npm install
 
+# Install Google Chrome
+RUN \
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+    apt-get update && \
+    apt-get install -y google-chrome-stable && \
+    wget https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64.deb && \
+    dpkg -i dumb-init_*.deb
+
 COPY . .
 
 EXPOSE 3000
--- src/index.js
+++ src/index.js
@@ -1,34 +1,90 @@
 const garie_plugin = require('garie-plugin')
+const fs = require('fs-extra');
 const path = require('path');
 const config = require('../config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const serveIndex = require('serve-index');
 
-const myGetFile = async (options) => {
-    options.fileName = '<my_garie_plugin>.txt';
-    const file = await garie_plugin.utils.helpers.getNewestFile(options);
-    return getResults(file);
-}
+const { launchChromeAndRunLighthouse, createReport } = require('./utils');
+
+const filterResults = (data = {}) => {
+    const { categories = {}, audits = {} } = data;
+
+    const { metrics = {} } = audits;
+    const { details = {} } = metrics;
+    const { items = [] } = details;
+    const metricItems = items[0] || {};
+
+    const report = {};
+
+    for (const categoryName in categories) {
+        if (!Object.prototype.hasOwnProperty.call(categories, categoryName)) {
+            continue;
+        }
+
+        const category = categories[categoryName];
+        report[`${category.id}-score`] = Math.round(category.score * 100);
+    }
+    for (const metricItem in metricItems) {
+        if (!Object.prototype.hasOwnProperty.call(metricItems, metricItem)) {
+            continue;
+        }
+
+        // For now don't report on any observered metrics
+        if (metricItem.indexOf('observed') === -1) {
+            const metric = metricItems[metricItem];
+            report[metricItem] = metricItems[metricItem];
+        }
+    }
+
+    const auditData = ['errors-in-console', 'time-to-first-byte', 'interactive', 'redirects'];
+
+    auditData.forEach(key => {
+        const { rawValue } = audits[key] || {};
+        if (rawValue !== undefined) {
+            report[key] = rawValue;
+        }
+    });
+
+    var cleanReport = {};
+    Object.keys(report).forEach(key => {
+        const rawValue = report[key];
+        if (rawValue !== undefined){
+            cleanReport[key] = rawValue;
+        }
+    });
+    return cleanReport;
+};
+
 
 const myGetData = async (item) => {
     const { url } = item.url_settings;
     return new Promise(async (resolve, reject) => {
         try {
-            const { extra_option } = item.url_settings;
-            const { reportDir } = item;
+            const lighthouse =
+                (await launchChromeAndRunLighthouse(url, {
+                    extends: 'lighthouse:default'
+                })) || {};
 
-            const options = { script: path.join(__dirname, './my_script.sh'),
-                        url: url,
-                        reportDir: reportDir,
-                        params: [ extra_option ],
-                        callback: myGetFile
-                    }
-            data = await garie_plugin.utils.helpers.executeScript(options);
+            console.log(`Successfully got data for ${url}`);
 
-// my code to get the data for a url
+            const { reportDir } = item
+            const reportFolder = garie_plugin.utils.helpers.reportDirNow(reportDir);
 
+            const resultsLocation = path.join(reportFolder, `/lighthouse.html`);
+
+            const report = await createReport(lighthouse.lhr);
+
+            fs.outputFile(resultsLocation, report)
+                .then(() => console.log(`Saved report for ${url}`))
+                .catch(err => {
+                     console.log(err)
+                });
+
+            const data = filterResults(lighthouse.lhr);
             resolve(data);
+
         } catch (err) {
             console.log(`Failed to get data for ${url}`, err);
             reject(`Failed to get data for ${url}`);
