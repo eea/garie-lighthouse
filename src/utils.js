dd6817988306b19adbb7be06d8d2d73dbf8b7f06
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-07-03 16:15:05 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -44,7 +44,6 @@ const launchChromeAndRunLighthouse = async (url, userConfig = {}, useFasterConne
     const defaultConfig = {
       preset: 'lighthouse:default',
       settings: {
-        onlyCategories: ['performance'],
         maxWaitForFcp: 15000,
         maxWaitForLoad: 35000,
         skipAudits: ['uses-http2'],
