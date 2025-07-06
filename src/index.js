7982e50c70b245083266a2f0d1d430c9cd83148c
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-07-07 00:30:14 +0300

    old roots

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const config = require('../config');
 
-const { getAndParseLighthouseData } = require('./utils');
+const { launchChromeAndRunLighthouse, createReport } = require('./utils');
 
 
 
@@ -14,7 +14,120 @@ const { getAndParseLighthouseData } = require('./utils');
 
 
 
-const myGetData = require('./myGetData');
+const filterResults = (data = {}, fasterInternetConnection) => {
+    const { categories = {}, audits = {} } = data;
+
+    const { metrics = {} } = audits;
+    const { details = {} } = metrics;
+    const { items = [] } = details;
+    const metricItems = items[0] || {};
+
+    const report = {};
+
+    for (const categoryName in categories) {
+        if (!Object.prototype.hasOwnProperty.call(categories, categoryName)) {
+            continue;
+        }
+
+        const category = categories[categoryName];
+        report[`${category.id}-score`] = Math.round(category.score * 100);
+    }
+    for (const metricItem in metricItems) {
+        if (!Object.prototype.hasOwnProperty.call(metricItems, metricItem)) {
+            continue;
+        }
+
+        if (metricItem.indexOf('observed') === -1) {
+            report[metricItem] = metricItems[metricItem];
+        }
+    }
+
+    const auditData = ['errors-in-console', 'time-to-first-byte', 'interactive', 'redirects'];
+
+    auditData.forEach(key => {
+        const { rawValue } = audits[key] || {};
+        if ((rawValue !== undefined) && (rawValue !== null)){
+            report[key] = rawValue;
+        }
+    });
+
+    var cleanReport = {};
+    Object.keys(report).forEach(key => {
+        const rawValue = report[key];
+        if (rawValue !== undefined){
+            if (fasterInternetConnection === true) {
+                cleanReport[key + '_fast'] = rawValue;
+            } else {
+                cleanReport[key] = rawValue;
+            }
+        }
+    });
+    return cleanReport;
+};
+
+const getAndParseLighthouseData = async(item, url, fasterInternetConnection, reportFolder) => {
+    try {
+        const lighthouse =
+            (await launchChromeAndRunLighthouse(url, {
+                extends: 'lighthouse:default'
+            }, fasterInternetConnection)) || {};
+
+        if (fasterInternetConnection) {
+            console.log(`Successfully got fast data for ${url}`);
+        } else {
+            console.log(`Successfully got default data for ${url}`);
+        }
+
+        let resultsLocation = "";
+        if (fasterInternetConnection) {
+            resultsLocation = path.join(reportFolder, `/lighthouse_fast.html`);
+        } else {
+            resultsLocation = path.join(reportFolder, `/lighthouse.html`);
+        }
+
+        const report = await createReport(lighthouse.lhr);
+        fs.outputFile(resultsLocation, report)
+        .then(() => console.log(`Saved report for ${url}`))
+        .catch(err => {
+            console.log(err)
+        });
+
+        const data = filterResults(lighthouse.lhr, fasterInternetConnection);
+        return data;
+    } catch (err) {
+        console.log(`Failed to run lighthouse for ${url}`, err);
+        throw err;
+    }
+}
+
+const myGetData = async (item) => {
+    let regex = /^core[.][0-9]*$/
+    fs.readdirSync('.')
+      .filter(f => regex.test(f))
+      .map(f => fs.removeSync(f))
+    
+    const { url } = item.url_settings;
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log("Start:", url);
+
+            const { reportDir } = item
+            const reportFolder = garie_plugin.utils.helpers.reportDirNow(reportDir);
+
+            const data_fast = await getAndParseLighthouseData(item, url, true, reportFolder);
+            const data = await getAndParseLighthouseData(item, url, false, reportFolder);
+            const full_data = {
+                ...data,
+                ...data_fast
+            };
+            resolve(full_data);
+
+        } catch (err) {
+            console.log(`Failed to get data for ${url}`, err);
+            reject(`Failed to get data for ${url}`);
+        }
+    });
+};
 
 console.log("Start");
 console.log("Config maxCpus:", config?.plugins?.lighthouse?.maxCpus);
--- src/utils.js
+++ src/utils.js
@@ -1,78 +1,29 @@
 const chromeLauncher = require('chrome-launcher');
-const path = require('path');
-const fs = require('fs-extra');
 
 const chromeFlags = [
-	'--disable-gpu',
-	'--headless',
-	'--no-zygote',
-	'--no-sandbox',
-	'--disable-dev-shm-usage',
-	'--memory-pressure-off',
-	'--disable-features=VizDisplayCompositor',
-	'--disable-background-timer-throttling',
-	'--disable-background-networking',
-	'--disable-backgrounding-occluded-windows',
-	'--disable-breakpad',
-	'--disable-component-extensions-with-background-pages',
-	'--disable-extensions',
-	'--disable-features=TranslateUI',
-	'--disable-ipc-flooding-protection',
-	'--disable-renderer-backgrounding',
-	'--force-color-profile=srgb',
-	'--metrics-recording-only',
-	'--no-first-run',
+    '--disable-gpu',
+    '--headless',
+    '--no-zygote',
+    '--no-sandbox',
+    '--headless',
     '--collect.settings.maxWaitForFcp="450000"'
 ];
 
-// Simple sequential Chrome management like the old version
-let chromeQueue = [];
-let chromeInUse = false;
-
-const waitForChrome = async () => {
-  return new Promise((resolve) => {
-    if (!chromeInUse) {
-      chromeInUse = true;
-      resolve();
-    } else {
-      chromeQueue.push(resolve);
-    }
-  });
+const lighthouseOptions = {
+    throttling: {
+        rttMs: 40,
+        throughputKbps: 10*1024,
+        cpuSlowdownMultiplier: 1,
+        requestLatencyMs: 0,
+        downloadThroughputKbps: 0,
+        uploadThroughputKbp: 0
+    },
+    onlyCategories: ['performance']
 };
-
-const releaseChrome = () => {
-  chromeInUse = false;
-  if (global.gc) {
-    global.gc();
-  }
-  // Process next in queue
-  if (chromeQueue.length > 0) {
-    const next = chromeQueue.shift();
-    chromeInUse = true;
-    next();
-  }
-};
-
-const fasterInternetOptions = {
-  throttling: {
-    rttMs: 40,
-    throughputKbps: 10 * 1024,
-    cpuSlowdownMultiplier: 1,
-    requestLatencyMs: 0,
-    downloadThroughputKbps: 0,
-    uploadThroughputKbps: 0 
-  },
-  // Disable heavy audits for faster connection tests
-
-};
-
 
 const launchChromeAndRunLighthouse = async (url, config, fasterInternetConnection) => {
-    await waitForChrome();
-    
     let chrome;
     let result = [];
-    
     try {
         chrome = await chromeLauncher.launch({ chromeFlags });
 
@@ -83,14 +34,13 @@ const launchChromeAndRunLighthouse = async (url, config, fasterInternetConnectio
 
         if (fasterInternetConnection === true) {
             flags = {
-                ...fasterInternetOptions,
+                ...lighthouseOptions,
                 ...flags
             };
         }
 
         const lighthouse = (await import('lighthouse')).default;
         result = await lighthouse(url, flags, config);
-        
         await chrome.kill();
     } catch (err) {
         console.log("Error appeared while running lighthouse", err);
@@ -102,113 +52,17 @@ const launchChromeAndRunLighthouse = async (url, config, fasterInternetConnectio
             console.log("Error appeared after lighthouse crashed and tried to kill chrome", e);
         }
         throw err;
-    } finally {
-        releaseChrome();
     }
-    
     return result;
 };
+
 const createReport = async (results) => {
     const { ReportGenerator } = await import('lighthouse/report/generator/report-generator.js');
     return ReportGenerator.generateReportHtml(results);
 };
 
-const filterResults = (data = {}, fasterInternetConnection) => {
-    const { categories = {}, audits = {} } = data;
-
-    const { metrics = {} } = audits;
-    const { details = {} } = metrics;
-    const { items = [] } = details;
-    const metricItems = items[0] || {};
-
-    const report = {};
-
-    for (const categoryName in categories) {
-        if (!Object.prototype.hasOwnProperty.call(categories, categoryName)) {
-            continue;
-        }
-
-        const category = categories[categoryName];
-        report[`${category.id}-score`] = Math.round(category.score * 100);
-    }
-    for (const metricItem in metricItems) {
-        if (!Object.prototype.hasOwnProperty.call(metricItems, metricItem)) {
-            continue;
-        }
-
-        // For now don't report on any observered metrics
-        if (metricItem.indexOf('observed') === -1) {
-            report[metricItem] = metricItems[metricItem];
-        }
-    }
-
-    const auditData = ['errors-in-console', 'time-to-first-byte', 'interactive', 'redirects'];
-
-    auditData.forEach(key => {
-        const { rawValue } = audits[key] || {};
-        if ((rawValue !== undefined) && (rawValue !== null)){
-            report[key] = rawValue;
-        }
-    });
-
-    var cleanReport = {};
-    Object.keys(report).forEach(key => {
-        const rawValue = report[key];
-        if (rawValue !== undefined){
-            if (fasterInternetConnection === true) {
-                cleanReport[key + '_fast'] = rawValue;
-            } else {
-                cleanReport[key] = rawValue;
-            }
-        }
-    });
-    return cleanReport;
-};
-
-
-const getAndParseLighthouseData = async(item, url, fasterInternetConnection, reportFolder) => {
-    try {
-        const lighthouse =
-            (await launchChromeAndRunLighthouse(url, {
-            extends: 'lighthouse:default',
-            settings: {
-                onlyCategories: ['performance', 'pwa', 'accessibility', 'best-practices', 'seo']
-            }
-            }, fasterInternetConnection)) || {};
-
-        if (fasterInternetConnection) {
-            console.log(`Successfully got fast data for ${url}`);
-        } else {
-            console.log(`Successfully got default data for ${url}`);
-        }
-
-
-        let resultsLocation = "";
-        if (fasterInternetConnection) {
-            resultsLocation = path.join(reportFolder, `/lighthouse_fast.html`);
-        } else {
-            resultsLocation = path.join(reportFolder, `/lighthouse.html`);
-        }
-
-        const report = await createReport(lighthouse.lhr);
-        await fs.outputFile(resultsLocation, report);
-        console.log(`Saved report for ${url}`);
-
-        const data = filterResults(lighthouse.lhr, fasterInternetConnection);
-      
-       
-        
-        
-        return data;
-    } catch (err) {
-        console.log(`Failed to run lighthouse for ${url}`, err);
-        throw err;
-    }
-}
 
 module.exports = {
-	launchChromeAndRunLighthouse,
-	createReport,
-	filterResults,
-	getAndParseLighthouseData
+    launchChromeAndRunLighthouse,
+    createReport
 }
